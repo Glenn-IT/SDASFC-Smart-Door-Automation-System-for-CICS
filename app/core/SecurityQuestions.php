735639e10fcc0d24b779933a7cab7f5572201e13
Glenn-IT <glenard2308@gmail.com>
2026-07-06 11:44:52 +0800

    Add SecurityQuestions class with predefined security question list

---
+++ app/core/SecurityQuestions.php
@@ -0,0 +1,12 @@
+<?php
+
+class SecurityQuestions
+{
+    public const LIST = [
+        "What is your mother's maiden name?",
+        "What was the name of your first pet?",
+        "What elementary school did you attend?",
+        "What is your favorite book?",
+        "In what city were you born?",
+    ];
+}
